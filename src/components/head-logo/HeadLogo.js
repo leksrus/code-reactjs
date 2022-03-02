bbd0136c6ea65f3c66bec70eb03077d582bbeeb2
Oleksiy Kotylev <okotylev@galiciaseguros.com.ar>
2022-03-02 14:09:26 -0300

    se agrega nav bar con boostrap

---
+++ src/components/head-logo/HeadLogo.js
@@ -0,0 +1,12 @@
+
+
+function HeadLogo() {
+    return (
+        <h1>
+            <img className="logo-img" src="/images/logo.png" alt="logo" style={ { width: 50, height: 50}}/>
+            &nbsp;Tech Market
+        </h1>
+    );
+}
+
+export default HeadLogo;
